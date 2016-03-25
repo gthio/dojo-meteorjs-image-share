d4827ce2aa14ade946020bf0186a98bbd0d55ed2
gthio <gunawanthio@outlook.com>
2016-03-25 08:22:37 +0800

    Sending data to templates with helpers

--- image_share.js
+++ image_share.js
@@ -1,23 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  var img_data = {
+      img_src : "meteor.jpg",
+      img_alt : "meteor pic"
+  };        
+  
+  Template.images.helpers(img_data);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
