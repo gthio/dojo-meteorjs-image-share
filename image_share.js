1a1c5df3721e2c41e2c5c58426803785b67c570d
gthio <gunawanthio@outlook.com>
2016-03-25 08:33:51 +0800

    Sending data to templates with helpers - as an array

--- image_share.js
+++ image_share.js
@@ -1,10 +1,19 @@
 if (Meteor.isClient) {
-  var img_data = {
-      img_src : "meteor.jpg",
-      img_alt : "meteor pic"
-  };        
+  var img_data = [
+      {
+        img_src : "meteor_1.jpg",
+        img_alt : "meteor 1 pic"
+      },
+      {
+        img_src : "meteor_2.jpg",
+        img_alt : "meteor 2 pic"
+      },
+      {
+        img_src : "meteor_3.jpg",
+        img_alt : "meteor 3 pic"
+      }];        
   
-  Template.images.helpers(img_data);
+  Template.images.helpers({images: img_data});
 }
 
 if (Meteor.isServer) {
