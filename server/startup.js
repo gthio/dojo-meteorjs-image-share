61f56834332f50e97b1804a72dd7dfb834dceffd
gthio <gunawanthio@outlook.com>
2016-03-28 22:27:54 +0800

    Make site more secure

--- server/startup.js
+++ server/startup.js
@@ -1,17 +1,14 @@
-if (Meteor.isServer){
-    
-    Meteor.startup(function(){
-       if (Images.find().count() == 0){
+Meteor.startup(function(){
+    if (Images.find().count() == 0){
            
-           for (var i = 1; i <= 3 ; i++)
-           {
-               Images.insert({
-                   img_src: "meteor_" + i + ".jpg",
-                   img_alt: "meteor " + i + " pic"
-               });
-           }
+        for (var i = 1; i <= 3 ; i++)
+        {
+            Images.insert({
+                img_src: "meteor_" + i + ".jpg",
+                img_alt: "meteor " + i + " pic"
+            });
+        }
            
-           console.log("startup.js says: " + Images.find().count());
-       } 
-    });
-}
+        console.log("startup.js says: " + Images.find().count());
+    } 
+});
