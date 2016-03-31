94c5bcd0a483945e17470d2555969cc782ef9d2e
gthio <gunawanthio@outlook.com>
2016-03-31 22:31:14 +0800

    Better routing

--- client/main.js
+++ client/main.js
@@ -1,10 +1,37 @@
 
+Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+});
+
 Router.route('/', function () {
-  this.render('navbar');
+  this.render('welcome', {
+      to:"main"
+  });
 });
 
 Router.route('/images', function () {
-  this.render('images');
+
+  this.render('navbar', {
+      to:"navbar"
+  });
+  
+  this.render('images', {
+      to:"main"
+  });
+});
+
+Router.route('/image/:_id', function () {
+
+  this.render('navbar', {
+      to:"navbar"
+  });
+  
+  this.render('image', {
+      to:"main",
+      data:function(){
+          return Images.findOne({_id:this.params._id});
+      }
+  });
 });
 
 Session.set("imageLimit", 4);
