5124dc9132ed41b4c9c99eb402a70539e93a9a87
gthio <gunawanthio@outlook.com>
2016-04-01 23:33:40 +0800

    Code style update

--- server/startup.js
+++ server/startup.js
@@ -1,14 +1,15 @@
 Meteor.startup(function(){
-    if (Images.find().count() == 0){
-           
-        for (var i = 1; i <= 3 ; i++)
-        {
-            Images.insert({
-                img_src: "meteor_" + i + ".jpg",
-                img_alt: "meteor " + i + " pic"
-            });
-        }
-           
-        console.log("startup.js says: " + Images.find().count());
-    } 
+  
+  if (Images.find().count() == 0){
+
+    for (var i = 1; i <= 3 ; i++){
+      
+      Images.insert({
+        img_src: "meteor_" + i + ".jpg",
+        img_alt: "meteor " + i + " pic"
+        });
+    }
+
+    console.log("startup.js says: " + Images.find().count());
+  } 
 });
